1b36e3eee39ffc887b024d91104fc53ff777ddd2
ulempicka <urszula.lempicka@gmail.com>
2020-06-03 20:02:38 +0200

    added zad2

---
+++ Zadanie2.java
@@ -0,0 +1,8 @@
+public class Zadanie2 {
+    public static void main (String... args){
+        System.out.println("   *   ");
+        System.out.print("  ***  \n ***** \n*******\n");
+        System.out.println("   *   ");
+        System.out.println("   *   ");
+    }
+}
